8709c23289bbd05942b4bb23d382e4c5a379732c
timokrapf <timo-krapf@web.de>
2018-07-30 16:00:39 +0200

    Test

--- app/src/main/java/com/example/timokrapf/sic_smartindexcards/StartActivity.java
+++ app/src/main/java/com/example/timokrapf/sic_smartindexcards/StartActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class StartActivity extends AppCompatActivity {
 
+    private String timo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
