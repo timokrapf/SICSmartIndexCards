405dc91fe498461785b679bfb888e8764505888f
timokrapf <timo-krapf@web.de>
2018-08-01 13:11:32 +0200

    fragment klappt ganz gut

--- app/src/main/java/com/example/timokrapf/sic_smartindexcards/AddButtonFragment.java
+++ app/src/main/java/com/example/timokrapf/sic_smartindexcards/AddButtonFragment.java
@@ -1,6 +1,5 @@
 package com.example.timokrapf.sic_smartindexcards;
 
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,42 +7,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 
-public class AddSubjectFragment extends Fragment {
+public class AddButtonFragment extends Fragment {
 
-    OnAddSubjectButtonClicked mCallback;
+    OnAddButtonFragmentClicked mCallback;
 
-    public AddSubjectFragment(){
+    public AddButtonFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.fragment_add_subject, container, false);
-        Button button = (Button) view.findViewById(R.id.final_add_subject_button);
+        View v = inflater.inflate(R.layout.fragment_add_button, container, false);
+        Button button = (Button) v.findViewById(R.id.add_subject_button_id);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText text = (EditText) view.findViewById(R.id.subject_edit_text);
-                String newSubject = text.getText().toString();
-                mCallback.addSubjectButtonClicked(newSubject);
+                mCallback.addButtonFragmentClicked();
             }
         });
-        return view;
+        return v;
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         try {
-            mCallback = (OnAddSubjectButtonClicked) activity;
+            mCallback = (OnAddButtonFragmentClicked) activity;
         } catch (ClassCastException e) {
             e.printStackTrace();
         }
     }
 
-    public interface OnAddSubjectButtonClicked {
-        void addSubjectButtonClicked(String subject);
+    public interface OnAddButtonFragmentClicked {
+         void addButtonFragmentClicked();
     }
-
 }
--- app/src/main/java/com/example/timokrapf/sic_smartindexcards/StartActivity.java
+++ app/src/main/java/com/example/timokrapf/sic_smartindexcards/StartActivity.java
@@ -1,7 +1,7 @@
 package com.example.timokrapf.sic_smartindexcards;
 
 
-import android.support.v4.app.Fragment;
+
 import android.support.v4.app.FragmentActivity;
 
 import android.support.v4.app.FragmentTransaction;
@@ -9,37 +9,40 @@ import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
-
-
-public class StartActivity extends FragmentActivity {
+public class StartActivity extends FragmentActivity implements AddButtonFragment.OnAddButtonFragmentClicked, AddSubjectFragment.OnAddSubjectButtonClicked{
 
     private ListView listView;
-    private TextView emptyListText;
-    private Button addSubjectButton, subjectButton, scheduleButton;
-
+    private Button subjectButton, scheduleButton;
+    private AddSubjectFragment addSubjectFragment;
+    private AddButtonFragment addButtonFragment;
+    private EditText addSubjectText;
+    private String newSubject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         initUI();
+        initStartFragment();
         initButtons();
         setClickListener();
     }
 
     private void initUI() {
       listView = (ListView) findViewById(R.id.subjec_list_id);
-      emptyListText = (TextView) findViewById(R.id.empty_list_id);
-      listView.setEmptyView(emptyListText);
+      addSubjectText = (EditText) findViewById(R.id.subject_edit_text);
+
+
     }
 
     private void initButtons() {
         subjectButton = (Button) findViewById(R.id.subject_button_id);
-        addSubjectButton = (Button) findViewById(R.id.add_subject_button_id);
         scheduleButton = (Button) findViewById(R.id.schedule_planner_button_id);
         subjectButton.setEnabled(false);
     }
@@ -51,24 +54,40 @@ public class StartActivity extends FragmentActivity {
                 scheduleButtonClicked();
             }
         });
-        addSubjectButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addSubjectButtonClicked();
-            }
-        });
     }
 
-    private void addSubjectButtonClicked() {
-            AddSubjectFragment addSubjectFragment = new AddSubjectFragment();
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.replace(R.id.fragment_container, addSubjectFragment);
-            transaction.commit();
-    }
 
     private void scheduleButtonClicked() {
 
     }
 
+    private void initStartFragment() {
+        addButtonFragment = new AddButtonFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.fragment_container, addButtonFragment);
+        transaction.commit();
+    }
 
+    @Override
+    public void addButtonFragmentClicked() {
+        addSubjectFragment = new AddSubjectFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, addSubjectFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+    }
+
+    @Override
+    public void addSubjectButtonClicked(String subject) {
+        if(subject.equals("")) {
+            Toast.makeText(getApplicationContext(), getString(R.string.toast_for_no_subject), Toast.LENGTH_SHORT).show();
+        } else {
+            newSubject = subject;
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_container, addButtonFragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+    }
 }
